3574dec79ab193a68c143231aa6452971b4cb7f5
unknown <awesomeriptide@gmail.com>
2019-04-01 17:27:59 +0800

    Pushed with l̶o̶v̶e̶ shove.

--- DigitRecognitionRunner.java
+++ DigitRecognitionRunner.java
@@ -1,19 +1,5 @@
-/**
- *
- */
-
-/**
- * @author delgadomatac
- *
- */
-
 public class DigitRecognitionRunner {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		DigitRecognitionFrame frame = new DigitRecognitionFrame("");
 		// frame.setSize(800, 600);
 		frame.setVisible(true);
